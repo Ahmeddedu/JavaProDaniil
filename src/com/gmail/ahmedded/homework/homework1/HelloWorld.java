dc0995816cc7bc72b374775f5d8eb46a015fd567
Ahmeddedu <156914766+Ahmeddedu@users.noreply.github.com>
2025-06-24 01:50:57 +0300

    HomeWork 1.1 Make first basic comit with Hello World

---
+++ src/com/gmail/ahmedded/homework/homework1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.gmail.ahmedded.homework.homework1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        for(int i = 0; i<= 100; i++){
+            System.out.println("Hello World for - " + i);
+        }
+    }
+}
