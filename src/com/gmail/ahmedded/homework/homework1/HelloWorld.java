f115fd5d28440967101e56ff4697ed061cbe8dd8
Ahmeddedu <156914766+Ahmeddedu@users.noreply.github.com>
2025-06-24 17:41:17 +0300

    HomeWork 1.1 Male one message for 100 HelloWorld

--- src/com/gmail/ahmedded/homework/homework1/HelloWorld.java
+++ src/com/gmail/ahmedded/homework/homework1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         for(int i = 0; i<= 100; i++){
             System.out.println("Hello World for - " + i);
         }
+        System.out.println("100 Hello World wow :)");
     }
 }
