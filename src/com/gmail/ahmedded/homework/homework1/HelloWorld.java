211283733739adcde499a94e16ad02fa0e540f98
Danil Danonovich <danillpp2000@gmail.com>
2025-06-24 17:45:23 +0300

    HomeWork 1.1 Remake 100 HelloWorld on 1000

--- src/com/gmail/ahmedded/homework/homework1/HelloWorld.java
+++ src/com/gmail/ahmedded/homework/homework1/HelloWorld.java
@@ -2,7 +2,7 @@ package com.gmail.ahmedded.homework.homework1;
 
 public class HelloWorld {
     public static void main(String[] args){
-        for(int i = 0; i<= 100; i++){
+        for(int i = 0; i<= 1000; i++){
             System.out.println("Hello World for - " + i);
         }
         System.out.println("100 Hello World wow :)");
