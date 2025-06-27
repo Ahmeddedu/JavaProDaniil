31e23905d73a02f7fecb9b95eed6902ba909db64
Ahmeddedu <danillpp2000@gmail.com>
2025-06-27 18:26:54 +0300

    HomeWork 2.1 Make new git experience :)

--- src/com/gmail/ahmedded/homework/homework2/OnlyForGit.java
+++ src/com/gmail/ahmedded/homework/homework2/OnlyForGit.java
@@ -1,4 +1,7 @@
 package com.gmail.ahmedded.homework.homework2;
 
 public class OnlyForGit {
+    public static void main (String[] args){
+        System.out.println("New git experience");
+    }
 }
