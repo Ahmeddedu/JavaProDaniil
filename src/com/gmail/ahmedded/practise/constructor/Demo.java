8d69fe5db1c25f6023e2eab838a60f10a3554694
Ahmeddedu <danillpp2000@gmail.com>
2025-07-14 10:14:23 +0300

    Practise getter setters constructor

---
+++ src/com/gmail/ahmedded/practise/constructor/Demo.java
@@ -0,0 +1,10 @@
+package com.gmail.ahmedded.practise.constructor;
+
+public class Demo {
+    public static void main(String[] args){
+        Baza people1 = new Baza("Ahmediksik" , 380503500);
+
+        people1.toString();
+
+    }
+}
---
+++ src/com/gmail/ahmedded/practise/getSetConstructor/Demo.java
@@ -0,0 +1,33 @@
+package com.gmail.ahmedded.practise.getSetConstructor;
+
+public class Demo {
+    public static void main(String[] args){
+        Bazik car1 = new Bazik("White" , 8999 , 4);
+        car1.toString();
+
+        car1.setWheel(6);
+        car1.toString();
+
+        Bazik car2 = new Bazik("White" , 8999 , 4);
+
+        Bazik car3 = car2;
+        car2.toString();
+        car3.toString();
+
+        System.out.println(car2 == car3);
+
+        Bazik car10 = new Bazik("White" , 8999 , 4);
+        car10.toString();
+        Bazik car20 = new Bazik("White" , 8999 , 4);
+        car20.toString();
+
+        System.out.println(car10 == car20);
+        System.out.println(car10.hashCode());
+        System.out.println(car20.hashCode());
+
+        System.out.println(car10.getColor());
+        car10.setColor("Grey");
+        System.out.println(car10.getColor());
+
+    }
+}
