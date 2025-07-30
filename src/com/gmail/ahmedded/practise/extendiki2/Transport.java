fe921d9129553acb499ccff28c2048a5a8812859
Ahmeddedu <danillpp2000@gmail.com>
2025-07-30 19:13:21 +0300

    Practise extends i so little forgot

---
+++ src/com/gmail/ahmedded/practise/extendiki2/Transport.java
@@ -0,0 +1,10 @@
+package com.gmail.ahmedded.practise.extendiki2;
+
+public class Transport {
+    public void Start(){
+        System.out.println("Transport start");
+    }
+    public void Bibikat(){
+
+    }
+}
