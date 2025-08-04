195505b263b7a1581f980ba6676724efc7e2b8b8
Ahmeddedu <danillpp2000@gmail.com>
2025-08-04 15:11:29 +0300

    Claen project

--- BazaMaven/src/main/java/com/gmail/ahmedded/practise/Exaptionika/Ups.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.ahmedded.practise.Exaptionika;
-
-public class Ups extends RuntimeException {
-    public Ups(String message) {
-        super(message);
-    }
-}
--- BazaMaven/src/main/java/com/gmail/ahmedded/practise/enumik/Week.java
+++
@@ -1,6 +0,0 @@
-package com.gmail.ahmedded.practise.enumik;
-
-public enum Week {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
-}
-
