8e7831ffdfe25680eddfbdbe3829fb83e785ac7b
Ahmeddedu <danillpp2000@gmail.com>
2025-08-07 15:51:09 +0300

    HomeWork 12.1 Working with threads

---
+++ BazaMaven/src/main/java/com/gmail/ahmedded/homework/homework12/DataRepository.java
@@ -0,0 +1,8 @@
+package com.gmail.ahmedded.homework.homework12;
+
+public class DataRepository {
+
+    public String[] getData() {
+        return new String[] {"orange", "apple", "plum", "mango"};
+    }
+}
