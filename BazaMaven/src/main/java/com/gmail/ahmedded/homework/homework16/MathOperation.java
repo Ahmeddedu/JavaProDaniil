f884f7738e2542e672ccb0c3c5e6f5e104777a17
Ahmeddedu <156914766+Ahmeddedu@users.noreply.github.com>
2025-09-08 23:24:44 +0300

    HomeWork 16.1

---
+++ BazaMaven/src/main/java/com/gmail/ahmedded/homework/homework16/MathOperation.java
@@ -0,0 +1,7 @@
+package com.gmail.ahmedded.homework.homework16;
+
+@FunctionalInterface
+public interface MathOperation {
+    int operate(int a, int b);
+}
+
---
+++ BazaMaven/src/main/java/com/gmail/ahmedded/homework/homework16/StringManipulator.java
@@ -0,0 +1,7 @@
+package com.gmail.ahmedded.homework.homework16;
+
+@FunctionalInterface
+public interface StringManipulator {
+    String manipulate(String input);
+}
+
